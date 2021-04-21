364e25c4e98c824a7f8de1b61b74e04fae683dda
destroylord <masapin68@gmail.com>
2021-04-21 10:51:34 +0700

    menambahkan tugas minggu ke 5

---
+++ Minggu ke-5/Vertexdfs.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package DFS;
+
+
+public class Vertexdfs {
+    public char label;
+    public boolean wasVisited;
+    
+    public Vertexdfs(char lab) {
+        label = lab; 
+        wasVisited = false;
+    }
+    
+}
+
